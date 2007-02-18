d49b34302a70ad948ce9cbf5778b1b39adb4286d
Pedro J. Ruiz Lopez <holzplatten@es.gnu.org>
2007-02-18 19:35:04 +0000

    Upgrade to gettext 0.16.1

--- intl/version.c
+++ intl/version.c
@@ -1,5 +1,5 @@
-/* OS dependent parts of libintl.
-   Copyright (C) 2001-2002, 2006 Free Software Foundation, Inc.
+/* libintl library version.
+   Copyright (C) 2005 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify it
    under the terms of the GNU Library General Public License as published
@@ -16,11 +16,11 @@
    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
    USA.  */
 
-#if defined __CYGWIN__
-# include "intl-exports.c"
-#elif defined __EMX__
-# include "os2compat.c"
-#else
-/* Avoid AIX compiler warning.  */
-typedef int dummy;
+#ifdef HAVE_CONFIG_H
+# include <config.h>
 #endif
+
+#include "libgnuintl.h"
+
+/* Version number: (major<<16) + (minor<<8) + subminor */
+int libintl_version = LIBINTL_VERSION;
