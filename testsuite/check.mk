582103c67e6718b484e052fc135cba7056eeaf0a
Jim Meyering <meyering@redhat.com>
2008-10-23 22:44:10 +0200

    tests: add testing framework, and tests for the new option
    * NEWS (New features): Mention the new option.
    * src/mkid.c (main): Adjust a diagnostic.
    * src/xtokid.c (main): Adjust a diagnostic.
    * bootstrap.conf (gnulib_modules): Add perl and posix_shell
    * build-aux/check.mk: New file, from coreutils.
    * testsuite/Coreutils.pm: Likewise.
    * testsuite/CuTmpdir.pm: Likewise.
    * testsuite/check.mk: Likewise.
    * testsuite/envvar-check: Likewise.
    * testsuite/files0-from: New test, based on one from coreutils.
    * testsuite/Makefile.am (TESTS): Add files0-from.
    (TESTS_ENVIRONMENT): Remove definition.  Now it's in check.mk.
    (EXTRA_DIST): Add Coreutils.pm CuTmpdir.pm envvar-check and check.mk.
    Include $(srcdir)/check.mk.

---
+++ testsuite/check.mk
@@ -0,0 +1,75 @@
+# Include this file at the end of each tests/*/Makefile.am.
+# Copyright (C) 2007, 2008 Free Software Foundation, Inc.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+built_programs = \
+  (cd $(top_builddir)/src && MAKEFLAGS= $(MAKE) -s built_programs.list)
+
+# Note that the first lines are statements.  They ensure that environment
+# variables that can perturb tests are unset or set to expected values.
+# The rest are envvar settings that propagate build-related Makefile
+# variables to test scripts.
+TESTS_ENVIRONMENT =				\
+  tmp__=$$TMPDIR; test -d "$$tmp__" || tmp__=.;	\
+  . $(srcdir)/envvar-check;			\
+  TMPDIR=$$tmp__; export TMPDIR;		\
+  shell_or_perl_() {				\
+    if grep '^\#!/usr/bin/perl' "$$1" > /dev/null; then			\
+      if $(PERL) -e 'use warnings' > /dev/null 2>&1; then		\
+	grep '^\#!/usr/bin/perl -T' "$$1" > /dev/null && T_=T || T_=;	\
+        $(PERL) -w$$T_ -I$(srcdir) -MCoreutils				\
+	      -M"CuTmpdir qw($$tst)" -- "$$1";	\
+      else					\
+	echo 1>&2 "$$tst: configure did not find a usable version of Perl," \
+	  "so skipping this test";		\
+	(exit 77);				\
+      fi;					\
+    else					\
+      $(SHELL) "$$1";				\
+    fi;						\
+  };						\
+  export					\
+  LOCALE_FR='$(LOCALE_FR)'			\
+  LOCALE_FR_UTF8='$(LOCALE_FR_UTF8)'		\
+  abs_top_builddir='$(abs_top_builddir)'	\
+  abs_top_srcdir='$(abs_top_srcdir)'		\
+  abs_srcdir='$(abs_srcdir)'			\
+  built_programs="`$(built_programs)`"		\
+  host_os=$(host_os)				\
+  host_triplet='$(host_triplet)'		\
+  srcdir='$(srcdir)'				\
+  top_srcdir='$(top_srcdir)'			\
+  CONFIG_HEADER='$(abs_top_builddir)/lib/config.h' \
+  CU_TEST_NAME=`basename '$(abs_srcdir)'`,$$tst	\
+  AWK='$(AWK)'					\
+  EGREP='$(EGREP)'				\
+  EXEEXT='$(EXEEXT)'				\
+  MAKE=$(MAKE)					\
+  PACKAGE_BUGREPORT='$(PACKAGE_BUGREPORT)'	\
+  PACKAGE_VERSION=$(PACKAGE_VERSION)		\
+  PERL='$(PERL)'				\
+  PREFERABLY_POSIX_SHELL='$(PREFERABLY_POSIX_SHELL)' \
+  REPLACE_GETCWD=$(REPLACE_GETCWD)		\
+  PATH="$(abs_top_builddir)/src$(PATH_SEPARATOR)$(abs_top_srcdir)/src$(PATH_SEPARATOR)$$PATH" \
+  ; shell_or_perl_
+
+TEST_LOGS = $(TESTS:=.log)
+
+# Parallel replacement of Automake's check-TESTS target.
+SUFFIXES =
+include $(top_srcdir)/build-aux/check.mk
+
+VERBOSE = yes
